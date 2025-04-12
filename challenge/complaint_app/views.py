69104dfa33e0c3a385753a9beb062dcde7b63961
Ali-Fares <faresa0202@gmail.com>
2025-04-12 19:35:27 -0400

    refactored and abstracted complaint retrieval logic

--- challenge/complaint_app/views.py
+++ challenge/complaint_app/views.py
@@ -8,28 +8,32 @@ from django.db.models import Count
 from datetime import date
 # Create your views here.
 
-# rememger to refactor code to eliminate redundancy, e.g. retrieving all complaints for a district can be abstracted into a single function and called when necessary
+# refactored complaint retrieval by district code logic by abstracting into a helper function to eliminate redundancy
+
+def retrieve_complaints_by_district(council_member_district):
+  # assuming district numbers dont exceed 99
+  if (council_member_district < 10):
+    # Pad with 0 for single-digit districts to match format like 'NYCC07'
+    council_member_district_as_str = f'0{council_member_district}'
+    complaints = Complaint.objects.annotate(
+      acct_dist_num=Substr('account', 5, Length('account') - 4)
+    ).filter(acct_dist_num=council_member_district_as_str)
+  else:
+    # No need to annotate if no zero-padding is needed
+    council_member_district_as_str = str(council_member_district)
+    complaints = Complaint.objects.filter(account__endswith=council_member_district_as_str)
+  return complaints
+  
 
 class ComplaintViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
   serializer_class = ComplaintSerializer
   def list(self, request):
     # Get all complaints from the user's district
-    # assuming district numbers dont exceed 99
-
     # Get the user's district and normalize it
     council_member_dist_as_int = int(self.request.user.userprofile.district)
 
-    if council_member_dist_as_int < 10:
-      # Pad with 0 for single-digit districts to match format like 'NYCC07'
-      council_member_dist_as_str = f'0{council_member_dist_as_int}'
-      complaints = Complaint.objects.annotate(
-        acct_dist_num=Substr('account', 5, Length('account') - 4)
-      ).filter(acct_dist_num=council_member_dist_as_str)
-    else:
-      # No need to annotate if no zero-padding is needed 
-      council_member_dist_as_str = str(council_member_dist_as_int)
-      complaints = Complaint.objects.filter(account__endswith=council_member_dist_as_str)
+    complaints = retrieve_complaints_by_district(council_member_dist_as_int)
 
     serializer = ComplaintSerializer(complaints, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
@@ -41,14 +45,7 @@ class OpenCasesViewSet(viewsets.ModelViewSet):
 
     council_member_dist_as_int = int(self.request.user.userprofile.district)
 
-    if council_member_dist_as_int < 10:
-      council_member_dist_as_str = f'0{council_member_dist_as_int}'
-      complaints = Complaint.objects.annotate(
-        acct_dist_num=Substr('account', 5, Length('account') - 4)
-      ).filter(acct_dist_num=council_member_dist_as_str)
-    else:
-      council_member_dist_as_str = str(council_member_dist_as_int)
-      complaints = Complaint.objects.filter(account__endswith=council_member_dist_as_str)
+    complaints = retrieve_complaints_by_district(council_member_dist_as_int)
 
     openComplaints = complaints.filter(closedate__isnull=True)
     serializer = ComplaintSerializer(openComplaints, many=True)
@@ -60,14 +57,7 @@ class ClosedCasesViewSet(viewsets.ModelViewSet):
     # Get only complaints that are close from the user's district
     council_member_dist_as_int = int(self.request.user.userprofile.district)
 
-    if council_member_dist_as_int < 10:
-      council_member_dist_as_str = f'0{council_member_dist_as_int}'
-      complaints = Complaint.objects.annotate(
-        acct_dist_num=Substr('account', 5, Length('account') - 4)
-      ).filter(acct_dist_num=council_member_dist_as_str)
-    else:
-      council_member_dist_as_str = str(council_member_dist_as_int)
-      complaints = Complaint.objects.filter(account__endswith=council_member_dist_as_str)
+    complaints = retrieve_complaints_by_district(council_member_dist_as_int)
 
     closedComplaints = complaints.filter(closedate__lte=date.today())
     serializer = ComplaintSerializer(closedComplaints, many=True)
@@ -79,14 +69,7 @@ class TopComplaintTypeViewSet(viewsets.ModelViewSet):
     # Get the top 3 complaint types from the user's district
     council_member_dist_as_int = int(self.request.user.userprofile.district)
 
-    if council_member_dist_as_int < 10:
-      council_member_dist_as_str = f'0{council_member_dist_as_int}'
-      complaints = Complaint.objects.annotate(
-        acct_dist_num=Substr('account', 5, Length('account') - 4)
-      ).filter(acct_dist_num=council_member_dist_as_str)
-    else:
-      council_member_dist_as_str = str(council_member_dist_as_int)
-      complaints = Complaint.objects.filter(account__endswith=council_member_dist_as_str)
+    complaints = retrieve_complaints_by_district(council_member_dist_as_int)
 
     topThreeComplaints = complaints.values('complaint_type').annotate(totalComplaints=Count('id')).order_by('-totalComplaints')[:3]
     serializer = ComplaintSerializer(topThreeComplaints, many=True)
